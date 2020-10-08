e67343b5a8a5e32c8ee49af8dd806a10e5179232
Lars Eric Scheidler <lscheidler@liventy.de>
2020-10-08 13:16:17 +0200

    externalize images list

---
+++ images.rb
@@ -0,0 +1,28 @@
+def images 
+  [
+    {directory: 'docker-openjdk8', name: 'openjdk8'},
+    {directory: 'docker-jmxtrans-agent', name: 'jmxtrans-agent'},
+    {directory: 'docker-tomcat8', name: 'tomcat8'},
+    {directory: 'docker-service-wrapper', name: 'service-wrapper'},
+    {directory: 'docker-nodejs', name: 'nodejs'},
+    {directory: 'docker-ruby', name: 'ruby'},
+    {directory: 'docker-solr', name: 'solr'},
+    {directory: 'docker-jar', name: 'jar'},
+    {directory: 'docker-influxdb', name: 'influxdb'},
+    {directory: 'docker-vault', name: 'vault'},
+    {directory: 'docker-tomcat7', name: 'tomcat7'},
+    {directory: 'docker-service-wrapper', name: 'service-wrapper-tomcat7', parameter: "tomcat=tomcat7"},
+    {directory: 'docker-rabbitmq', name: 'rabbitmq'},
+    {directory: 'docker-mailslurper', name: 'mailslurper'},
+    {directory: 'docker-postgresql', name: 'postgresql'},
+    {directory: 'docker-postgresql-client', name: 'postgresql-client'},
+    {directory: 'docker-memcached', name: 'memcached'},
+    {directory: 'docker-busybox', name: 'busybox'},
+    {directory: 'docker-easy-rsa', name: 'easy-rsa'},
+    {directory: 'docker-mariadb', name: 'mariadb'},
+    {directory: 'docker-restic', name: 'restic'},
+    {directory: 'docker-openjdk11', name: 'openjdk11'},
+    {directory: 'docker-gradle', name: 'gradle'},
+    {directory: 'docker-maven', name: 'maven'},
+  ]
+end
